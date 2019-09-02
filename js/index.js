3977ab10da8318934f7ed99c6be757e9cd25ab57
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-09-02 16:06:43 -0400

    Initial commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,15 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//  Create selectors by using any of the DOM element's methods
+//Note that IDs have been used on all images. Use the IDs to update src path content
+
+let navAll = document.querySelectorAll('a');
+let nat = document.Selector('nav');
+let cta = document.querySelector('.cta');
+let ctaText = document.querySelector('.cta-text h1');
+let button = document.querySelector('.cta-text button');
+let content = document.querySelector('.text-content');
+let contact = document.querySelector('.contact');
+let footer = document.querySelector('footer');
